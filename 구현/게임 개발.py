772454cb005bb8ee7dd56240bfe41f406786cd24
Park Sanghyun <p981116@naver.com>
2022-01-20 21:13:40 +0900

    solved by powerful104

--- 구현/게임 개발.py
+++ 구현/게임 개발.py
@@ -3,28 +3,34 @@ A, B, d = list(map(int, input().split()))
 map_info = []
 
 #맵 구성
-map_info.append([0]*M)
 for _ in range(N):
-    map_info.append([0]+list(map(int, input().split()))+[0])
-map_info.append([0]*M)
+    map_info.append(list(map(int, input().split())))
 
+map_check = list(map_info)
 
 dir_num = [0,1,2,3]
 dir = [(-1,0),(0,1),(1,0),(0,-1)]
 
-count = 0
+count = 1
 nomove = 0
 
 while True:
-    nd = dir_num[d-1]
-    na = A + dir[nd][0]
-    nb = B + dir[nd][1]
+    d = dir_num[d-1]
+    na = A + dir[d][0]
+    nb = B + dir[d][1]
     
-    if map_info[na][nb] == 1:
+    if map_check[na][nb] == 0:
         A = na
         B = nb
         nomove = 0
+        map_check[na][nb] = 1
+        count += 1
     else:
         nomove += 1
     
-    d = dir_num[d-1]
+    if nomove == 4:
+        na = A - dir[d][0]
+        na = B - dir[d][1]
+        if map_info[na][nb] == 1:
+            break
+print(count)
